ab3fef3451418346e653eb885453bd470409c4bd
cchapman82 <43580831+cchapman82@users.noreply.github.com>
2019-10-27 01:45:29 -0500

    Update CleanSweeperRobot.java

--- CleanSweeperRobot.java
+++ CleanSweeperRobot.java
@@ -8,7 +8,7 @@ public class CleanSweeperRobot {
 		
 		System.out.println("Welcome to Group7's Clean Sweeper Robot!");
 
-		System.out.println("Please enter file to read room from.");
+		System.out.println("Please enter file to read room from.  Choose from : \n simple.json\n initial.json");
 		Scanner in = new Scanner(System.in);
 		String name = in.next();
 		ConfigMngr configMngr = new ConfigMngr(name);
