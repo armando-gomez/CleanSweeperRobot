33dbaf1f92547ad7e2ff5a93b01c28471132e389
mowshe <6mikeski13@gmail.com>
2019-10-29 23:17:32 -0500

    Added more unit tests for cell

--- CellTest.java
+++ CellTest.java
@@ -17,8 +17,8 @@ public class CellTest {
 		System.out.println(testCell.getType());
 		assertEquals(testCell.getType(), "bare");
 	}
-	
-	//TODO set each direction set and get
+		
+	// Set <direction> to already existing opposite direction
 	
 	@Test 
 	public void setForwardTrueTest() {
@@ -43,7 +43,7 @@ public class CellTest {
 	
 	@Test 
 	public void setBackFalseTest() {
-		Cell testCell = new Cell(true, false, false, false, 0, "bare");
+		Cell testCell = new Cell(false, true, false, false, 0, "bare");
 		testCell.setBack(false);
 		assertEquals(testCell.getBack(),false);
 	}
@@ -57,7 +57,7 @@ public class CellTest {
 	
 	@Test 
 	public void setRightFalseTest() {
-		Cell testCell = new Cell(true, false, false, false, 0, "bare");
+		Cell testCell = new Cell(false, false, true, false, 0, "bare");
 		testCell.setRight(false);
 		assertEquals(testCell.getRight(),false);
 	}
@@ -71,17 +71,76 @@ public class CellTest {
 	
 	@Test 
 	public void setLeftFalseTest() {
-		Cell testCell = new Cell(true, false, false, false, 0, "bare");
+		Cell testCell = new Cell(false, false, false, true, 0, "bare");
 		testCell.setLeft(false);
 		assertEquals(testCell.getLeft(),false);
 	}
 	
+	// Set <direction> to already existing direction 
 	
+	@Test 
+	public void setForwardTrueTest2() {
+		Cell testCell = new Cell(true, false, false, false, 0, "bare");
+		testCell.setForward(true);
+		assertEquals(testCell.getForward(),true);
+	}
 	
-
+	@Test 
+	public void setForwardFalseTest2() {
+		Cell testCell = new Cell(false, false, false, false, 0, "bare");
+		testCell.setForward(false);
+		assertEquals(testCell.getForward(),false);
+	}
+	
+	@Test 
+	public void setBackTrueTest2() {
+		Cell testCell = new Cell(false, false, false, false, 0, "bare");
+		testCell.setBack(true);
+		assertEquals(testCell.getBack(),true);
+	}
+	
+	@Test 
+	public void setBackFalseTest2() {
+		Cell testCell = new Cell(true, false, false, false, 0, "bare");
+		testCell.setBack(false);
+		assertEquals(testCell.getBack(),false);
+	}
+	
+	@Test 
+	public void setRightTrueTest2() {
+		Cell testCell = new Cell(false, false, false, false, 0, "bare");
+		testCell.setRight(true);
+		assertEquals(testCell.getRight(),true);
+	}
+	
+	@Test 
+	public void setRightFalseTest2() {
+		Cell testCell = new Cell(false, false, false, false, 0, "bare");
+		testCell.setRight(false);
+		assertEquals(testCell.getRight(),false);
+	}
+	
+	@Test 
+	public void setLeftTrueTest2() {
+		Cell testCell = new Cell(false, false, false, true, 0, "bare");
+		testCell.setLeft(true);
+		assertEquals(testCell.getLeft(),true);
+	}
+	
+	@Test 
+	public void setLeftFalseTest2() {
+		Cell testCell = new Cell(false, false, false, false, 0, "bare");
+		testCell.setLeft(false);
+		assertEquals(testCell.getLeft(),false);
+	}
 	
 	//TODO assert each one capital letter
 	
+//	@Test(expected = InvalidEntryException.class)
+//	public void testCapitalTypeSet() {
+//		Cell testCell = new Cell(false, false, false, false, 0, "bare");
+//		testCell.setType("Bare");
+//	}
 	
 	
 	//TODO assert different type
